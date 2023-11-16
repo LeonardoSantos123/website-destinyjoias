bd5a3ae837a4567d38ddd14f061dddc6b6e2358a
Leonardo Santos <62783168+LeonardoSantos123@users.noreply.github.com>
2023-11-15 21:06:52 -0300

    Update Header.js

--- JS/Header.js
+++ JS/Header.js
@@ -1,29 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon = document.getElementById('menu-icon');
-  const menuOverlay = document.getElementById('menu-overlay');
-  const overlayRight = document.getElementById('overlay-right');
-
-  menuIcon.addEventListener('click', function () {
-      console.log('Menu icon clicked'); // Verifique se este log aparece no console
-      menuOverlay.classList.toggle('show-menu-overlay');
-      overlayRight.classList.toggle('show-overlay-right');
-
-      if (overlayRight.classList.contains('show-overlay-right')) {
-          overlayRight.style.display = 'block';
-          setTimeout(function () {
-              overlayRight.style.opacity = '1';
-          }, 10);
-      } else {
-          overlayRight.style.opacity = '0';
-          setTimeout(function () {
-              overlayRight.style.display = 'none';
-          }, 300);
-      }
+    const menuIcon = document.getElementById('menu-icon');
+    const menuOverlay = document.getElementById('menu-overlay');
+    const overlayRight = document.getElementById('overlay-right');
+    const body = document.body;
+  
+    menuIcon.addEventListener('click', function () {
+        console.log('Menu icon clicked');
+        menuOverlay.classList.toggle('show-menu-overlay');
+        overlayRight.classList.toggle('show-overlay-right');
+  
+        if (menuOverlay.classList.contains('show-menu-overlay')) {
+            body.classList.add('no-scroll'); 
+            overlayRight.style.display = 'block';
+            setTimeout(function () {
+                overlayRight.style.opacity = '1';
+            }, 10);
+        } else {
+            body.classList.remove('no-scroll'); 
+            overlayRight.style.opacity = '0';
+            setTimeout(function () {
+                overlayRight.style.display = 'none';
+            }, 300);
+        }
+    });
+  
+    overlayRight.addEventListener('click', function () {
+        console.log('Overlay right clicked');
+        menuOverlay.classList.remove('show-menu-overlay');
+        overlayRight.classList.remove('show-overlay-right');
+        body.classList.remove('no-scroll'); 
+    });
   });
-
-  overlayRight.addEventListener('click', function () {
-      console.log('Overlay right clicked'); // Verifique se este log aparece no console
-      menuOverlay.classList.remove('show-menu-overlay');
-      overlayRight.classList.remove('show-overlay-right');
-  });
-});

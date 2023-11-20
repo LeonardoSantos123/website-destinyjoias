aa320f7e6ef33167b856ec567d4d919255372baf
Leonardo Santos <62783168+LeonardoSantos123@users.noreply.github.com>
2023-11-20 19:50:40 -0300

    Update Tela de Carregamento.js

--- JS/Tela de Carregamento.js
+++ JS/Tela de Carregamento.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var loader = document.querySelector('.profile-main-loader');
 
-    // Configura um temporizador para adicionar a classe 'fade-out' após 2 segundos
     setTimeout(function () {
         loader.classList.add('fade-out');
         document.getElementById('loading-page').style.display = 'block';
-    }, 1000); // Tempo ajustado para 2 segundos (você pode ajustar conforme necessário)
+    }, 1000);
 });

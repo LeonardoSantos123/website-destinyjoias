c0d91646bdff858e49ead8bb202ad65d7c54235f
Leonardo Santos <62783168+LeonardoSantos123@users.noreply.github.com>
2023-12-06 04:30:53 -0300

    Update Pagamento.js

--- JS/Pagamento.js
+++ JS/Pagamento.js
@@ -28,13 +28,13 @@ new Vue({
         if (number.match(re) != null) return "visa";
   
         re = new RegExp("^(34|37)");
-        if (number.match(re) != null) return "amex";
+        if (number.match(re) != null) return "inter";
+
+        re = new RegExp("^6011");
+        if (number.match(re) != null) return "nubank";
   
         re = new RegExp("^5[1-5]");
         if (number.match(re) != null) return "mastercard";
-  
-        re = new RegExp("^6011");
-        if (number.match(re) != null) return "discover";
         
         re = new RegExp('^9792')
         if (number.match(re) != null) return 'troy'
